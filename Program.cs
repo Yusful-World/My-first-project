49dbb7c168af4f294ae0c6f6bc778995ba813f5f
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2024-07-21 22:16:17 +0100

    My second commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace New_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World"); 
+            Console.WriteLine("Hello World");
         }
     }
 }
